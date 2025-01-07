2ef48d2f7da85628f4de9c7ca49ce7b0da6e5e0c
rwparrish <60946750+rwparrish@users.noreply.github.com>
2025-01-07 15:50:33 -0700

    style Gallery

--- client/src/components/Gallery.jsx
+++ client/src/components/Gallery.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import ArtworkCard from './ArtworkCard';
 
 
@@ -6,23 +6,22 @@ const Gallery = () => {
 
     const [artworks, setArtworks] = useState([]);
 
-    console.log(artworks);
-
     useEffect(() => {
         fetch('http://127.0.0.1:8000/api/artworks')
             .then(res => res.json())
             .then(data => setArtworks(data));
     }, []);
-    
 
-    const artworksToDisplay = artworks.map(artwork => 
-        { return <ArtworkCard key={artwork.id} artwork={artwork}/> });
+
+    const artworksToDisplay = artworks.map(artwork => { return <ArtworkCard key={artwork.id} artwork={artwork} /> });
 
     return (
-        <div className="gallery">
-            <h2>Gallery</h2>
-            {artworksToDisplay}
-        </div>
+        <section className="flex flex-col justify-center items-center h-auto py-12">
+            <div className="flex flex-col max-w-screen-xl justify-center items-center gap-8">
+                <h2>Gallery</h2>
+                {artworksToDisplay}
+            </div>
+        </section>
     );
 };
 
