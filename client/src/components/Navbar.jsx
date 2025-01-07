e0ae1885ae231189e7ef62e88a4b912cb39ad314
rwparrish <60946750+rwparrish@users.noreply.github.com>
2025-01-07 15:43:59 -0700

    style NavBar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,24 +3,26 @@ import {Link} from 'react-scroll'
 
 
 const Navbar = () => {
+
+    const linkStyles = "hover:underline cursor-pointer";
     return (
         <div className="flex flex-col items-center bg-white border-b border-darkest/10 drop-shadow-sm">
             <nav className="flex flex-row w-full justify-between items-center max-w-screen-2xl">
             <div className="navbar-logo">
-                <Link to="/">Colorful Blobs</Link>
+                <h1 className="text-2xl font-bold text-gray-800">Colorful Blobs</h1>
             </div>
             <ul className="flex flex-row gap-8 items-center justify-between w-1/3 px-6 py-6">
                 <li>
-                    <Link to="/home">Home</Link>
+                    <Link to="/home" className={linkStyles} >Home </Link>
                 </li>
                 <li>
-                    <Link to="/about">About</Link>
+                    <Link to="/about" className={linkStyles} >About</Link>
                 </li>
                 <li>
-                    <Link to="/gallery">Gallery</Link>
+                    <Link to="/gallery" className={linkStyles} >Gallery</Link>
                 </li>
                 <li>
-                    <Link to="/contact">Contact</Link>
+                    <Link to="/contact" className={linkStyles} >Contact</Link>
                 </li>
             </ul>
         </nav>
